b04009ff7124e45b9fdfd837d3272f3276929ba7
Marlon Vega <mvegach2013@gmail.com>
2019-05-24 23:46:41 -0600

    Arreglado selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -4,50 +4,44 @@
  */
 
 
-public class SelectionSort<T extends Comparable<T>> {
-    public SelectionSort(LinkedList<T> list){
-        LinkedList<T> lista = list;
-        SortSelection(lista);
-    }
-    public LinkedList SortSelection(LinkedList<T> lista){
-        int arrayLength = lista.getSize();
+public class SelectionSort<T extends Comparable<? super T>> {
 
-        for (int unsortIndex = arrayLength - 1; unsortIndex > 0; unsortIndex--) {
-            int largest = 0;
-            for (int i = 1; i <= unsortIndex; i++) {
-                if (lista.Comparar(lista.get(i), lista.get(largest))) {
-                    largest = i;
+    public void selectionSort(LinkedList<T> list) {
+        int size = list.getSize();
+
+        for (int i=0; i< size -1; i++) {
+            int min = i;
+            for (int j=i+1; j<size; j++) {
+                if (list.get(j).compareTo(list.get(min)) < 0) {
+                    min = j;
                 }
             }
-            lista = swap(lista, largest, unsortIndex);
+            swap(list, min, i);
         }
-        return lista;
-
     }
 
-        public LinkedList swap(LinkedList lista, int x, int y) {
-            T temp = (T) lista.get(x);
-            lista.set(x, lista.get(y));
-//        collection[x] = collection[y];
-            lista.set(y, temp);
-//        collection[y] = temp;
-            return lista;
-        }
-//        public static void main(String[] args) {
-//        LinkedList<Integer> list = new LinkedList<>();
-//        list.addLast(7);
-//        list.addLast(2);
-//        list.addLast(1);
-//        list.addLast(6);
-//        list.addLast(8);
-//        list.addLast(5);
-//        list.addLast(3);
-//        list.addLast(4);
-//        System.out.println(list);
-//        System.out.println("\n");
-//        SelectionSort<Integer> selection = new SelectionSort<>(list);
-//        selection.SortSelection(list);
-//
-//        System.out.println(list);
-//    }
+    private void swap(LinkedList<T> lista, int min, int i) {
+        T temp = lista.get(min);
+        lista.set(min, lista.get(i));
+        lista.set(i, temp);
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addLast(14);
+        list.addLast(2);
+        list.addLast(25);
+        list.addLast(3);
+        list.addLast(46);
+
+        System.out.println(list);
+
+        SelectionSort<Integer> sort = new SelectionSort<>();
+
+        sort.selectionSort(list);
+
+        System.out.println(list);
+
+    }
 }
