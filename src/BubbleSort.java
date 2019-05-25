f05e29b59ac9e301c45a28bc2e6d48c432f01ad4
Marlon Vega <mvegach2013@gmail.com>
2019-05-24 23:56:38 -0600

    Arreglado bubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -7,41 +7,35 @@
 
 public class BubbleSort<T extends Comparable<T>> {
 
-    public BubbleSort(LinkedList<T> list) { //Se recibe la linkedList
-        LinkedList<T> lista = list;
-        Sort(lista);
-    }
+    public void bubbleSort(LinkedList<T> list) {
+        int size = list.getSize();
 
-    public LinkedList Sort(LinkedList<T> lista) {
-        int n = lista.getSize(); //n es el tamanio de la lista
-        for (int i = 0; i < n - 1; i++) { // se recorre un elemento
-            for (int j = 0; j < n - i - 1; j++) {//se compara con otro  elemento
-                if (lista.Comparar(lista.get(j), lista.get(j + 1))) {//Se compara si el primero es mayor al segundo
-                    T temp = (T) lista.get(j); // Se asigna un temporal para guardar el contenido del primer elemento
-//                    lista.acces_index(j).setValue(lista.get(j+1));//se cambian los nodos entre otros
-//                    lista.acces_index(j+1).setValue(temp.getValue());
-                    lista.set(j, lista.get(j + 1));
-                    lista.set(j + 1, temp);
+        for (int i=0; i<size-1; i++) {
+            for (int j=0; j< size-i-1; j++) {
+                if (list.get(j).compareTo(list.get(j+1)) > 0) {
+                    T tmp = list.get(j);
+                    list.set(j, list.get(j+1));
+                    list.set(j+1, tmp);
                 }
             }
         }
-        return lista; //se retorna la nueva lista
     }
-//    public static void main(String[] args) {
-//        LinkedList<Integer> list = new LinkedList<>();
-//        list.addLast(7);
-//        list.addLast(2);
-//        list.addLast(1);
-//        list.addLast(6);
-//        list.addLast(8);
-//        list.addLast(5);
-//        list.addLast(3);
-//        list.addLast(4);
-//        System.out.println(list);
-//        System.out.println("\n");
-//        BubbleSort<Integer> bubble = new BubbleSort<>(list);
-//        bubble.Sort(list);
-//
-//        System.out.println(list);
-//    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addLast(105);
+        list.addLast(23);
+        list.addLast(14);
+        list.addLast(5);
+        list.addLast(13);
+
+        System.out.println(list);
+
+        BubbleSort<Integer> sort = new BubbleSort<>();
+
+        sort.bubbleSort(list);
+
+        System.out.println(list);
+    }
 }
